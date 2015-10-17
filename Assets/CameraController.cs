c12ab420552cf02f4a62d1d653afe04af70ae4fe
Mauricio Manuel Cavalleri Sergent <maurimanuel92@hotmail.es>
2015-10-17 19:19:13 +0100

    Cámara orbital y avance en level 1
    La cámara orbital tiene soporte para mando y avance en la creación del
    nivel 1

---
+++ Assets/CameraController.cs
@@ -0,0 +1,22 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class CameraController : MonoBehaviour
+{
+    public GameObject target;
+    public float speed;
+    public float distance;
+    private float currentAnglex = 0;
+    private float currentAngley = 35;
+
+    void LateUpdate()
+    {
+        currentAnglex += Input.GetAxis("Mouse X") * speed * Time.deltaTime;
+        currentAngley += Input.GetAxis("Mouse Y") * speed * Time.deltaTime;
+
+        Quaternion q = Quaternion.Euler(currentAngley, currentAnglex, 0);
+        Vector3 direction = q * Vector3.forward;
+        transform.position = target.transform.position - direction * distance;
+        transform.LookAt(target.transform.position);
+    }
+}
